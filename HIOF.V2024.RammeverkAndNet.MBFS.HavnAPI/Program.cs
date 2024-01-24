96e1941ff2cff8778274b479fc4a345c451f7723
bardh <bardh@BARDH>
2024-01-24 15:18:39 +0100

    Add project files.

---
+++ HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
