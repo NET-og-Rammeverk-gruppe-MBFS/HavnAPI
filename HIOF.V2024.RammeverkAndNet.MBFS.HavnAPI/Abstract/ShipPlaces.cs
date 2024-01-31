06f13e3009bda4cdb88012cfdc98afd4b94a9d79
mahamoudibrahim <mahamod.ibrahim@gmail.com>
2024-01-31 16:12:55 +0100

    La til variabler, konstruktør, og tomme metoder ved hjelp av UML diagrammet

--- HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Abstract/ShipPlaces.cs
+++ HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Abstract/ShipPlaces.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 namespace HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Abstract
 {
-	abstract class ShipPlaces
+	public abstract class ShipPlaces
 	{
-		public ShipPlaces()
+		private string Name { get; set; }
+		private int Spaces { get; set; }
+		private List<Ship> Ships { get; }
+		
+
+		public ShipPlaces(string ShipName, int ShipSpaces)
+		{
+			Name = ShipName;
+			Spaces = ShipSpaces;
+		}
+
+		public void AddShip(Ship ship)
 		{
 		}
+
+		public Ship MoveShip(int id)
+		{
+			return null;
+		}
+
+		public bool AvailableSpace()
+		{
+			return false;
+		}
+
+
+
+
 	}
 }
 
