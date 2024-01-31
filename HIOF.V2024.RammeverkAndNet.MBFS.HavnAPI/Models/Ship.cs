a3395cd33107040a8292479f9efa8e2e6f0888a0
Sadik Topalaj <sadiktop@hotmail.no>
2024-01-31 15:53:46 +0100

    lagde konstuktører på ship

--- HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Models/Ship.cs
+++ HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Models/Ship.cs
@@ -1,15 +1,37 @@
+using HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Enums;
 using System;
 
 public class Ship
 {
     public int ShipId { get; set; }
-    public int ContainerCapacity { get; set; }
+    public ShipType Type { get; set; }
 
-    public Ship{}
+    public int Cargospace { get; set; }
+    public int Passangerspace { get; set; }
+    public float Tankcapacity { get; set; }
 
-    public Ship(int shipId, int containerCapatity)
+    public Ship(int shipId, ShipType type, int cargospace, int passangerspace, float tankcapacity)
     {
         ShipId = shipId;
-        ContainerCapacity = containerCapatity;
+        Type = type;
+        Cargospace = cargospace;
+        Passangerspace = passangerspace;
+        Tankcapacity = tankcapacity;
+    }
+
+    public void simulationInformation()
+    {
+        switch (Type)
+        {
+            case ShipType.Cargo:
+                Console.Writeline($"Ship {ShipId} is a {Type} with a cargocapacity of {Cargospace}");
+                break;
+            case ShipType.Passenger:
+                Console.Writeline($"Ship {ShipId} is a {Type} with a passangercapacity of {Passangerspace}");
+                break;
+            case ShipType.Tank:
+                Console.Writeline($"Ship {ShipId} is a {Type} with a tankcapacity of {Tankcapacity}");
+                break;
+        }
     }
 }
