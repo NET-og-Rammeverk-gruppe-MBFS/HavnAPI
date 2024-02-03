99ee0c3d2eeea7597cafe68e049b1b6276c40534
Fredbro <fredbro@hotmail.no>
2024-02-03 12:58:48 +0100

    La til override metoder i anchorage

--- HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Models/Anchorage.cs
+++ HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Models/Anchorage.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Abstract;
 namespace HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Models
 
@@ -7,6 +8,22 @@ namespace HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Models
 public class Anchorage : ShipPlaces
 {
     public Anchorage(string Name, int Spaces) : base(name, spaces)
+    {
+    }
+
+    /// <summary>
+    /// Flytter skipet fra plass til en annen plass
+    /// <summary>
+    /// <param name="id">Id til skipet som flyttes</param>
+    public override Ship MoveShip(int id)
+    {
+    }
+
+    /// <summary>
+    /// Legger til et  skip til ankerplassen
+    /// <summary>
+    /// <param name="ship">Skipet som skal legges til</param>
+    public override void AddShip(Ship ship)
     {
     }
 }
