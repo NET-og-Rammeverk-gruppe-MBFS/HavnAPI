e72f050cc7630d2c857583443401ad5703d93b51
mahamoudibrahim <mahamod.ibrahim@gmail.com>
2024-02-07 14:22:07 +0100

    La til Main static i HavnAPI for å kunne bruke api-et

---
+++ HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Models;
+using HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Abstract;
+using HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Services;
+namespace HIOF.V2024.RammeverkAndNet.MBFS.TestClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+
