aff53faa8356fe13edf260bbc4af90e7fb1ecb22
Sadik Topalaj <sadiktop@hotmail.no>
2024-03-04 19:13:16 +0100

    ferdig med å lage api'et til gruppen, nå gjennstår det bare å finne endringer

---
+++ HIOF.V2024.RammeverkAndNet.MBFS.EndringTheRemnantsAPI/Terminal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIOF.V2024.RammeverkAndNet.MBFS.EndringTheRemnantsAPI
+{
+    internal class Terminal
+    {
+        public List<Item> Items { get; private set; }
+        public int Capacity { get; private set; }
+
+        public event Action<Item> ItemRemoved;
+
+        public Terminal(int capacity){}
+        public void AddItem(Item item){}
+
+        public void RemoveItem(Item item){}
+
+        public List<Item> GetItems()
+        {
+            return Items;
+        }
+
+        public void ConfigureTerminal(int newCapacity) {}
+    }
+}
