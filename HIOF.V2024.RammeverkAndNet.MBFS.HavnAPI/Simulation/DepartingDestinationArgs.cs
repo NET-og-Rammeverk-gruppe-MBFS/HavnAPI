f892734a70f5a130d4225d662fb37ea90fd0f066
Fredbro <fredbro@hotmail.no>
2024-03-04 19:32:05 +0100

    Gjorde om DepartingDestination til DepartingDestinationArgs

---
+++ HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI/Simulation/DepartingDestinationArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HIOF.V2024.RammeverkAndNet.MBFS.HavnAPI.Simulation
+{
+	public class DepartingDestinationArgs : EventArgs
+	{
+		/// <summary>
+		/// Når skipet forlater sitt destinasjon
+		/// </summary>
+		public DepartingDestinationArgs()
+		{
+		}
+	}
+}
+
